a103342227f1aae898667e40a236d22c3f7f66a9
Vladislav Lyulin <lyulin.vlad26@yandex.ru>
2024-05-21 13:06:53 +0300

    Задание готово

--- task-4/bin/surf_dart_courses_template.dart
+++ task-4/bin/surf_dart_courses_template.dart
@@ -1,5 +1,30 @@
-import 'package:surf_dart_courses_template/surf_dart_courses_template.dart' as surf_dart_courses_template;
+typedef Strategy = double Function(
+    List<String> cardOnDesk, List<String> currentHand);
 
-void main(List<String> arguments) {
-  print('Hello world: ${surf_dart_courses_template.calculate()}!');
+class PokerPlayer {
+  List<String> _currentHand = ['King of clubs', 'Nine of hearts'];
+
+  double _surenessInWin = 0;
+
+  void calculateProbabilities(
+    List<String> cardOnDesk,
+    Strategy strategy,
+  ) =>
+      _surenessInWin = strategy(cardOnDesk, _currentHand);
+}
+
+void main() {
+  final opponent = PokerPlayer();
+
+  final Strategy fakeStrategy =
+      (List<String> cardOnDesk, List<String> currentHand) {
+    print('Карты противника: $cardOnDesk');
+
+    return 0.0;
+  };
+
+  opponent.calculateProbabilities(
+    ['Nine of diamonds', 'king of hearts'],
+    fakeStrategy,
+  );
 }
