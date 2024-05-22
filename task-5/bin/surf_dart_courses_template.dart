aaf1673c0cd94a858a2ba868eb95eaae81d13933
Vladislav Lyulin <lyulin.vlad26@yandex.ru>
2024-05-22 17:54:24 +0300

    Задание готово

--- task-5/bin/surf_dart_courses_template.dart
+++ task-5/bin/surf_dart_courses_template.dart
@@ -1,5 +1,69 @@
-import 'package:surf_dart_courses_template/surf_dart_courses_template.dart' as surf_dart_courses_template;
+abstract class Person {
+  String name;
 
-void main(List<String> arguments) {
-  print('Hello world: ${surf_dart_courses_template.calculate()}!');
+  Person(this.name);
+
+  void describe() {
+    print('Имя: $name');
+  }
+}
+
+abstract class IPlayer {
+  void play();
+}
+
+abstract class ICoach {
+  void coach();
+}
+
+class Gamer extends Person implements IPlayer {
+  String team;
+  int matchesPlayed;
+
+  Gamer(String name, this.team, this.matchesPlayed) : super(name);
+
+  @override
+  void play() {
+    print('$name играет за команду $team и сыграл $matchesPlayed матчей.');
+  }
+}
+
+class Coach extends Person implements ICoach {
+  String team;
+  int trophiesWon;
+
+  Coach(String name, this.team, this.trophiesWon) : super(name);
+
+  @override
+  void coach() {
+    print('$name тренирует команду $team и выиграл $trophiesWon трофеев.');
+  }
+}
+
+class RolePlayer extends Gamer {
+  String role;
+
+  RolePlayer(String name, String team, int matchesPlayed, this.role)
+      : super(name, team, matchesPlayed);
+
+  @override
+  void play() {
+    super.play();
+    print('$name играет роль $role.');
+  }
+}
+
+void main() {
+  Gamer gamer = Gamer("Александр", "TeamA", 50);
+  Coach coach = Coach("Игорь", "TeamA", 10);
+  RolePlayer rolePlayer = RolePlayer("Петр", "TeamB", 30, "Снайпер");
+
+  gamer.describe();
+  gamer.play();
+
+  coach.describe();
+  coach.coach();
+
+  rolePlayer.describe();
+  rolePlayer.play();
 }
